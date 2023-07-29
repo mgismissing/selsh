5e1c61e2607b58c6bfaf0933453af45ab28b88dc
mgismissing <130852204+mgismissing@users.noreply.github.com>
2023-07-29 23:36:53 +0200

    Update selsh.py

--- selsh.py
+++ selsh.py
@@ -1,55 +1,22 @@
-selcell = 0
-mincell = 0
-maxcell = 8
-cells = []
-debug = 1
+CODE = input()
+def read(item):
+	if CODE[item] == "#": hash(item)
+def hash(item):
+	act = []
+	act.append("FOR")
+	act.append(CODE[item + 1])
+	if CODE[item + 2] == ":":
+		act.append("IN")
+		if CODE[item + 3] == "r":
+			act.append("RANGE")
+			act.append(CODE[item + 4])
+		 act.append(CODE[item + 5])
+			act.append(CODE[item + 6])
+			if CODE[item + 7] == "@":
+				act.append("THEN")
+				read(item + 8)
+			else:
+				return "Error", "Syntax"
 
-def init(min, max, sel, src):
-    global selcell
-    global mincell
-    global maxcell
-    global cells
-    
-    selcell = sel
-    mincell = min
-    maxcell = max
-    cells = []
-
-    for i in range(min, max):
-        cells.append(src)
-
-init(0, 4, 0, 0)
-print("> Move by 1")
-print("< Move by -1")
-print("! Not gate")
-print("= Set to [IN   ]")
-print("? [OUT  ]")
-print("0 Set to 0")
-print("1 Set to 1")
-code = input("[CODE ] = ")
-
-print("[START]")
-try:
-    for i in range(len(code)):
-        char = code[i]
-        sel = cells[selcell]
-        if char == ">":
-            if not selcell >= maxcell - 1: selcell += 1
-        if char == "<":
-            if not selcell <= mincell: selcell -= 1
-        if char == "!":
-            if sel == 0: cells[selcell] = 1
-            else: cells[selcell] = 0
-        if char == "=":
-            user = input("[GET ?] = ")
-            cells[selcell] = 0 if user == "0" else 1
-        if char == "?":
-            print(f"[GET {sel}]")
-    if code == "":
-        print("[EMPTY]")
-    print("[END  ]")
-except KeyboardInterrupt:
-    print("[KSTOP]")
-    print("[EXIT ]")
-except:
-    print("[ERR  ]")
+act = []
+read(0)
